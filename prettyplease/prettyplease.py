9b75b97bf0b4004a574b207e922769376ee811e5
Isak Svensson <isak.svensson@chalmers.se>
2020-09-16 19:07:51 +0200

    corner: well-defined contours, grayscale/color option and new colors, printing 1-sigma levels, refactoring

--- prettyplease/prettyplease.py
+++ prettyplease/prettyplease.py
@@ -4,16 +4,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 
-def corner(data, bins=50, labels=None, title=None, show_estimates=True, figsize=(10,10)):
+def contour_levels(grid, percentiles=[0.68, 0.95, 0.997]):
+    """Compute contour levels for a gridded 2D posterior"""
+    sorted_ = np.flipud(np.sort(grid.ravel()))
+    pct = np.cumsum(sorted_) / np.sum(sorted_)
+    cutoffs = np.searchsorted(pct, np.array(percentiles) ** 2)
+    return np.sort(sorted_[cutoffs])
+
+def corner(data, bins=50, percentiles=[0.68, 0.95, 0.997], labels=None, title=None, show_estimates=True, fmt='.3f', grayscale=False, figsize=(10,10)):
     """Create a pretty corner plot."""
-    density_cmap = LinearSegmentedColormap.from_list(
-        "density_cmap", ['k', (1, 1, 1, 0)])
+    #density_cmap = LinearSegmentedColormap.from_list("density_cmap", ['k', (1, 1, 1, 0)])
+    colors = ['white', 'gray', 'black'] if grayscale else ['white', 'blue','purple']
+    density_cmap = LinearSegmentedColormap.from_list("density_cmap", colors=colors)
     ndim = data.shape[1]
     fig, axes = plt.subplots(nrows=ndim, ncols=ndim, squeeze=False, figsize=figsize)
+
+    # Upper triangle
     for col in range(ndim):
         for row in range(col):
-            # DO THIS FOR THE UPPER TRIANGLE
             axes[row, col].axis('off')
+    # Lower triangle
+    for col in range(ndim):
         for row in range(col+1, ndim):
             # DO THIS FOR THE LOWER TRIANGLE
             ax = axes[row, col]
@@ -22,23 +33,32 @@ def corner(data, bins=50, labels=None, title=None, show_estimates=True, figsize=
             x1 = data[:, col]
             x2 = data[:, row]
             hist = np.histogram2d(x1, x2, bins=bins)
-            ax.imshow(hist[0].T, origin='lower', cmap='binary')
-            #ax.contourf(hist[0].T, cmap='gist_yarg', levels=3, alpha=0.3)
-            ax.contour(hist[0].T, colors=['gray','black'], linewidths=0.8, levels=3, alpha=1.0)
+            h = hist[0].T
+            vmin = (np.max(h)-np.min(h)) / 50 + np.min(h) # Make low levels white
+            ax.contourf(h, cmap=density_cmap, levels=30, vmin=vmin)
+            contourlevels = contour_levels(hist[0].T, percentiles)
+            ax.contour(h, colors='gray', linewidths=0.8, levels=contourlevels, alpha=1.0)
+    # Diagonal
     for i in range(ndim):
-        # THE DIAGONAL
+        x = data[:, i].flatten()
         ax = axes[i, i]
-        ax.hist(data[:, i], bins=bins, color='black', histtype='stepfilled')
+        ax.hist(x, bins=bins, color='black', histtype='step')
         ax.set_xticks([])
         ax.set_yticks([])
         if show_estimates:
-            median = data[:, i].median()
-            ax.set_title(f'{median:.2f}')
-    for i in range(ndim):
-        # ADD LABELS
+            median = np.percentile(x, 50)
+            low = np.percentile(x, 16) - median
+            high = np.percentile(x, 84) - median
+            ax.set_title(rf'${median:{fmt}}_{{{low:{fmt}}}}^{{+{high:{fmt}}}}$')
+    # Bottom
+    for col in range(ndim):
         if labels is not None:
-            axes[-1, i].set_xlabel(labels[i])
-            axes[i, 0].set_ylabel(labels[i])
+            axes[-1, col].set_xlabel(labels[col])
+    # Left
+    for row in range(ndim):
+        if labels is not None:
+            axes[row, 0].set_ylabel(labels[row])
+    # Remove space between subplots
     fig.subplots_adjust(wspace=0, hspace=0)
     return fig
 
