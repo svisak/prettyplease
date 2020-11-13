bdd6adf10d37fc251d15beae984eb3e06d13169f
Isak Svensson <isak.svensson@chalmers.se>
2020-11-13 09:57:49 +0100

    Read version from git describe

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
+import subprocess
+
+cmd = ['git', 'describe', '--tags', '--abbrev=0']
+version = subprocess.run(cmd, stdout=subprocess.PIPE)
+version = version.stdout.decode('utf-8').strip()
 
 setup(
     name='prettyplease',
-    version='0.1',
+    version=version,
     description='Create plots.',
     author='Isak Svensson',
     author_email='isak.svensson@chalmers.se',
