21c20c4d4377d14b37fecfb0fd0ac80d2a9267ea
Isak Svensson <isak.svensson@chalmers.se>
2021-03-05 00:36:48 +0100

    Made contour_levels available from the outside world.

--- prettyplease/__init__.py
+++ prettyplease/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from .prettyplease import contour_levels
 from .prettyplease import corner
 from .prettyplease import compare_marginals
 from .prettyplease import traceplot
