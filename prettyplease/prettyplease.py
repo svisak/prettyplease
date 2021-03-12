Fixed a bug in the computation of contour levels. For some reason the levels were previously squared so that if the user input e.g. 0.5, a 0.25 level was computed. Also changed the 'quantiles' keyword to 'levels', partly because it's more correct and partly to ensure that no one uses the buggy version by mistake.

prettyplease/prettyplease.py
@@ -5,14 +5,14 @@ import matplotlib.pyplot as plt
 from matplotlib import ticker
 from matplotlib.colors import LinearSegmentedColormap
 
-def contour_levels(grid, quantiles=[0.68, 0.95, 0.997]):
+def contour_levels(grid, levels=[0.68, 0.95]):
     """Compute contour levels for a gridded 2D posterior"""
     sorted_ = np.flipud(np.sort(grid.ravel()))
     pct = np.cumsum(sorted_) / np.sum(sorted_)
-    cutoffs = np.searchsorted(pct, np.array(quantiles) ** 2)
+    cutoffs = np.searchsorted(pct, np.array(levels))
     return np.sort(sorted_[cutoffs])
 
-def corner(data, bins=50, quantiles=[0.68, 0.95, 0.997], labels=None, colors='blue', title=None, show_estimates=True, n_ticks=4, fmt='.3f', figsize=(10,10)):
+def corner(data, bins=50, levels=[0.68, 0.95], labels=None, colors='blue', title=None, show_estimates=True, n_ticks=4, fmt='.3f', figsize=(10,10)):
     """Create a pretty corner plot."""
     # Color scheme. If colors is a string then the color scheme is white and the specified color.
     # If colors is a list the user has completely specified the color scheme they want.
@@ -56,7 +56,7 @@ def corner(data, bins=50, quantiles=[0.68, 0.95, 0.997], labels=None, colors='bl
             extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
             vmin = (np.max(hist)-np.min(hist)) / 50 + np.min(hist) # Make low levels white
             ax.contourf(hist, extent=extent, cmap=density_cmap, levels=30, vmin=vmin)
-            contourlevels = contour_levels(hist, quantiles)
+            contourlevels = contour_levels(hist, levels)
             ax.contour(hist, extent=extent, colors='gray', linewidths=0.8, levels=contourlevels, alpha=1.0)
 
     # Bottom labels