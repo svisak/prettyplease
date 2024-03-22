13eef2df18781e9308633077f3f7540a94b00af8
Isak Svensson <isak.svensson@physik.tu-darmstadt.de>
2024-03-23 00:52:11 +0100

    Updated copyright year

--- src/prettyplease/prettyplease.py
+++ src/prettyplease/prettyplease.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Isak Svensson
+# Copyright 2021-2024 Isak Svensson
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
